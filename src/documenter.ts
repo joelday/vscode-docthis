104e9b1b275d8d362d8472d9069387112c75055e
Joel Day <joelday@gmail.com>
2016-02-07 23:25:39 -0800

    Remove @exports misusage

--- src/documenter.ts
+++ src/documenter.ts
@@ -307,8 +307,6 @@ export class Documenter implements vs.Disposable {
         
         node.modifiers.forEach(modifier => {
             switch (modifier.kind) {
-                case ts.SyntaxKind.ExportKeyword:
-                sb.appendLine("@exports"); return;
                 case ts.SyntaxKind.AbstractKeyword:
                 sb.appendLine("@abstract"); return;
                 case ts.SyntaxKind.ProtectedKeyword:
